83e9cec6f2a34ae0cfba65468ca14dde49ac9f68
xdiach00 <xdiach00@vutbr.cz>
2021-10-12 17:41:00 +0200

    Completed tasks for Red Hat

---
+++ app.py
@@ -0,0 +1,26 @@
+import sys
+import http.server
+import socketserver
+import subprocess
+
+class httpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path in ["/date", "/time"]:
+            self.path = "static" + self.path + ".html"
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        elif self.path == "/":
+            self.path = "static/index.html"
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
+conf = subprocess.run(["grep", 'port = ', "conf/mytime.conf"], stdout=subprocess.PIPE)
+if b"=" not in conf.stdout:
+    conf = subprocess.run(["grep", "port=", "conf/mytime.conf"], stdout=subprocess.PIPE)
+    if b"=" not in conf.stdout:
+        print("Could not read configuration file `conf/mytime.conf`")
+        sys.exit(1)
+port = conf.stdout.split(b"=")[1]
+
+socketserver.TCPServer.allow_reuse_address = True
+server = socketserver.TCPServer(("", int(port)), httpRequestHandler)
+server.serve_forever()
